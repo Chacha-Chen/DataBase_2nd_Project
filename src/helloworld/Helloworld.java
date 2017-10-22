9b2bc9fad626b970debc46b01c80bb510b7064e4
Chacha-Chen <chacha1997@sjtu.edu.cn>
2017-10-22 10:28:58 +0800

    thread 1 completed

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -8,38 +8,84 @@ package helloworld;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
   
+class Thread1 extends Thread{
+    String name;
+    public Thread1(String name){
+        this.name=name;
+    }
+    public void run(){
+        String sql="select AVG(runtime) from movies where year=1963;";
+        try(Connection conn = Helloworld.connect();
+            Statement statement  = conn.createStatement();
+            ResultSet rs1 = statement.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs1.next()) {
+                System.out.println(rs1.getDouble("AVG(runtime)"));
+            }
+        }
+    
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    }
+
+
+class Thread2 extends Thread{
+    String name;
+    public Thread2(String name){
+        this.name=name;
+    }
+    public void run(){
+        try{
+            for(int i=0;i<5;i++)
+                System.out.println("B: "+i);
+        }
+        catch( Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
    
   
 public class Helloworld {
      /**
      * Connect to a sample database
+     * @return 
      */
-    public static void connect() {
+     public static Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:/Users/chenchacha/db/prob1.db";
         Connection conn = null;
         try {
-            // db parameters           
-            // create a connection to the database
-            conn = DriverManager.getConnection("jdbc:sqlite:prob1.db");
-            
-            System.out.println("Connection to SQLite has been established.");
-            
+            conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
+        return conn;
     }
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        connect();
+        
+        try{
+            Thread1 mTh1= new Thread1("A");
+            Thread2 mTh2= new Thread2("B");
+            mTh1.start();
+            mTh2.start();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
